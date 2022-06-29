427073adb114d5ae0dd337d39650c7a580541b10
tony-ngo-03 <tonyngo@utexas.edu>
2022-06-29 16:16:21 -0500

    First commit
    - 2 buttons

---
+++ FirstAndroidProject/app/src/main/java/com/example/firstandroidproject/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.firstandroidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    public void disable(View v){
+        v.setEnabled(false);
+    }
+}
